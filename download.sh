76f41ad3973ec5febe1186a494bac1948633e6f8
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2020-05-05 15:38:47 +0900

    Downgrade to 3.3

--- download.sh
+++ download.sh
@@ -4,7 +4,7 @@
 if [ -f pycifrw_$VERSION_BASE.orig.tar.gz ]; then
   exit 127
 fi
-wget https://bitbucket.org/jamesrhester/pycifrw/downloads/PyCifRW-$VERSION_BASE.tar.gz
+wget https://sourceforge.net/projects/pycifrw.berlios/files/PyCifRW-$VERSION_BASE.tar.gz
 tar zxvf PyCifRW-$VERSION_BASE.tar.gz
 mv -f PyCifRW-$VERSION_BASE pycifrw_$VERSION_BASE
 tar zcvf pycifrw_$VERSION_BASE.orig.tar.gz pycifrw_$VERSION_BASE
