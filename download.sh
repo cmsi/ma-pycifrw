0f6a6014906afc9c72200a2a561970dcee94101f
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2020-05-05 11:11:55 +0900

    Initial release for 4.4

---
+++ download.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+. $(dirname $0)/path.sh
+
+if [ -f pycifrw_$VERSION_BASE.orig.tar.gz ]; then
+  exit 127
+fi
+wget https://bitbucket.org/jamesrhester/pycifrw/downloads/PyCifRW-$VERSION_BASE.tar.gz
+tar zxvf PyCifRW-$VERSION_BASE.tar.gz
+mv -f PyCifRW-$VERSION_BASE pycifrw_$VERSION_BASE
+tar zcvf pycifrw_$VERSION_BASE.orig.tar.gz pycifrw_$VERSION_BASE
+rm -rf pycifrw_$VERSION_BASE PyCifRW-$VERSION_BASE.tar.gz
